e0821e653b69467e89703fb1f6cc548bb5a1e2fb
mudit67 <bhardwajmudit006@gmail.com>
2020-08-27 19:24:58 +0530

    configured the index.html to serve html file in the public folder

--- node-http/index.js
+++ node-http/index.js
@@ -1,13 +1,43 @@
-var http = require('http');
-
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const { publicDecrypt } = require('crypto');
 const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeaders = ('Content-type', 'text/html');
-    res.end('<html><body><h1>Hello, World!</h1></body></html>');
+    console.log("Request for " + req.url  + " by method " + req.method);
+    if (req.method == 'GET') {
+        var fileUrl;
+        if(req.url == '/'){fileUrl = '/index.html'}
+        else {fileUrl = req.url}
+        var filePath = path.resolve('./public' + fileUrl);
+        const fileExt = path.extname(filePath);
+        if (fileExt == '.html'){
+            fs.exists(filePath, (exists) => {
+                if(!exists){
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end("<html><body><h1> Error code 404 :  "+ fileUrl +" not found.</h1></body></html>");
+                }
+                else {
+                    res.statusCode=200;
+                    res.setHeader('Content-Type', 'text/html');
+                    fs.createReadStream(filePath).pipe(res);
+                }
+            });
+        }
+        else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end("<html><body><h1> Error code 404 :  "+ fileUrl +" not an HTML file.</h1></body></html>");
+        }
+    }
+    else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end("<html><body><h1> Error code 404 :  "+ req.method +" not Supported.</h1></body></html>");
+    }
 });
 
 server.listen(port,hostname,() => {
